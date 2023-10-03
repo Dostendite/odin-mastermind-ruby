5b28814893c4256ed8945c0eea95e8e13483b5d1
dostendite <dostendite@outlook.es>
2023-10-03 15:19:03 +0200

    Create game class & Start code refactoring
    Doing a big overhaul of the code to allow
    for the game, player & computer classes
    to be born, also transferring responsibilities
    from the board class to the game class
    (which will manage it)

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,92 @@
 require 'colorize'
-require 'pry-byebug'
 
-# Mastermind game board
-class Board
-
-  attr_reader :key
-
+# Game object
+class Game
   def initialize
-    @board = (Array.new(12) { Array.new(8, ' ') })
+    @board = [board.new]
     @key = generate_key
-    @insert_counter = 0
+    @player = select_player
+    @turns_played = 0
   end
+
+  def select_player
+    # have player choose
+    # if they want to be the code maker
+    # or the code breaker
+  end
+  
+  def game_introduction
+    # introduce the player to the game
+    # and explain not only the
+    # gamemode, but also show the colors
+    # and indicators
+    select_player
+  end
+
+  def play_turn
+    @turns_played += 1
+
+  end
+
+  def play_game
+    # main game function
+  end
+
+  private
 
   def generate_key
     [1, 2, 3, 4, 5, 6].sample(4)
   end
+end
+
+# Game board (For Game object)
+class Board
+  attr_reader :key, :insert_counter
+  def initialize
+    @board = (Array.new(12) { Array.new(8, ' ') })
+    @colors = {'red' => 1, 'green' => 2, 'yellow' => 3,
+               'blue' => 4, 'magenta' => 5, 'white' => 6}
+    @insert_counter = 0
+  end
+
+  # play a turn and insert 4 colors
+  def insert_colors(color_array)
+    @insert_counter -= 1
+
+    color_array.each_with_index do |_color, idx|
+      @board[@insert_counter][idx] = color_array[idx]
+    end
+
+    inspect_colors
+    print_board
+    check_board_status
+  end
+
+  def print_board
+    system 'clear'
+    @board.each do |row|
+      # first 4 colors
+      row[0..3].each do |color|
+        print_color(color)
+      end
+      print ' | '
+      # guess indicators
+      row[4..7].each do |color|
+        print_color(color)
+      end
+      print "\n"
+    end
+  end
+
+  def check_board_status
+    if @insert_counter == -12
+      'Game over'
+    elsif @board[@insert_counter][0..3] == @key
+      'Victory'
+    end
+  end
+
+  private
 
   def print_color(color)
     case color
@@ -45,113 +117,28 @@ class Board
     end
   end
 
-  def print_board
-    system 'clear'
-    @board.each do |row|
-      # first 4 colors
-      row[0..3].each do |color|
-        print_color(color)
-      end
-      print ' | '
-      # guess indicators
-      row[4..7].each do |color|
-        print_color(color)
-      end
-      print "\n"
-    end
-  end
-
-  def check_board_status
-    if @insert_counter == -12
-      'Game over'
-    elsif @board[@insert_counter][0..3] == @key
-      'Victory'
-    end
-  end
-
   def colors_to_integers(color_array)
-    color_array.each_with_index do |color, idx|
-      case color
-      when 'red'
-        color_array[idx] = 1
-      when 'green'
-        color_array[idx] = 2
-      when 'yellow'
-        color_array[idx] = 3
-      when 'blue'
-        color_array[idx] = 4
-      when 'magenta'
-        color_array[idx] = 5
-      when 'white'
-        color_array[idx] = 6
-      end
+    # ['blue', 'green', 'yellow', 'red'] => [4, 2, 3, 1]
+    color_array.each_index do |idx|
+      color_array[idx] = @colors[color_array[idx]]
     end
     color_array
   end
 
-  def manual_insert_colors
-    print "Enter the four colors to play\n(separated by spaces): "
-    color_array = gets.chomp.split
-    colors_to_integers(color_array)
-    insert_colors(color_array)
-  end
-
-  # play a turn and insert 4 colors
-  def insert_colors(color_array)
-    @insert_counter -= 1
-
-    color_array.each_with_index do |_color, idx|
-      @board[@insert_counter][idx] = color_array[idx]
-    end
-
-    inspect_colors
-    print_board
-    check_board_status
-  end
-
-  # inspect the colors and return
-  # the feedback on the last values of
-  # the array
   def inspect_colors
-    # fix indicators always
-    # matching number
-    # (learn how they really work first though)
-    @board.each_with_index do |row|
-      indicators = Array.new(4)
+    @board.each do |row|
       0.upto(3) do |idx|
-        if row[idx] == @key[idx]
+        case row[idx]
+        when @key[idx]
           row[idx + 4] = '!'
-        elsif @key.include?(row[idx])
+        when @key.include?(row[idx])
           row[idx + 4] = '*'
         else
           row[idx + 4] = '?'
         end
       end
-      # if row[-1] == 'shuffled'
-      #   next
-      # else
-      #   row[4..7] = row[4..7].shuffle
-      #   row.push('shuffled')
-      # end
     end
   end
 end
 
-def gen_random_colors
-  [1, 2, 3, 4, 5, 6].sample(4)
-end
-
-board = Board.new
-game_on = true
-board.print_board
-
-while game_on
-  turn = board.manual_insert_colors
-  if turn == 'Game over'
-    puts "Game over, you're out of turns!"
-    break
-  elsif turn == 'Victory'
-    puts 'You guessed the code, you win!'
-    break
-  end
-end
+game = Game.new
